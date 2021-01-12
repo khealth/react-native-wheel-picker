83afa8e4da4826d2f9c576f6df359ebb39b3c3e6
Raimon Lapuente <raimon@khealth.ai>
2021-01-12 12:07:31 +0100

    remove formatting

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 
-import { Platform } from 'react-native';
-import { PickerIOS } from '@react-native-picker/picker';
+import { Platform } from 'react-native'
+import { PickerIOS } from '@react-native-picker/picker'
 
-import WheelCurvedPicker from './WheelCurvedPicker';
+import WheelCurvedPicker from './WheelCurvedPicker'
 
-module.exports = Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker;
+module.exports = (Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker)
